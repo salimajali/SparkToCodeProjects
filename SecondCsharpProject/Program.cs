707b17bee5d9332957e684ca177acf6d8a783292
Salima Juma <salimaj16@icloud.com>
2026-07-10 18:33:02 +0400

    My First Program

---
+++ SecondCsharpProject/Program.cs
@@ -0,0 +1,24 @@
+﻿namespace SecondCsharpProject
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+        // Salima's First C Program : 
+
+            Console.Write("Enter Your Name:  ");
+            string UserName = Console.ReadLine();
+
+            Console.Write("Enter Your Age:  ");
+            int UserAge =int.Parse(Console.ReadLine());
+
+            Console.Write("Enter Your Salary: ");
+            float UserSalary =float.Parse(Console.ReadLine());
+
+            Console.WriteLine("Welcome" + UserName + "Your Age: " + UserAge + "And Your Salary is: " + UserSalary);
+
+            Console.ReadKey();            
+        }
+    }
+}
